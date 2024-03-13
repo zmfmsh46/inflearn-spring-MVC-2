fb9ab1a52f90bf5d1d440395f5fce7c61133cda8
Yu Ho Yeon <zmfmsh46@gmail.com>
2024-03-13 18:16:07 +0900

    File Upload

---
+++ upload/src/main/java/hello/upload/file/FileStore.java
@@ -0,0 +1,62 @@
+package hello.upload.file;
+
+import hello.upload.domain.UploadFile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class FileStore {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    public String getFullPath(String filename) {
+        return fileDir + filename;
+    }
+
+    public List<UploadFile> storeFiles(List<MultipartFile> multipartFiles) throws IOException {
+        List<UploadFile> storeFileResult = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFiles) {
+            if (!multipartFile.isEmpty()) {
+                storeFileResult.add(storeFile(multipartFile));
+            }
+        }
+
+        return storeFileResult;
+    }
+
+    //스프링이 제공하는 MultipartFile을 받아서 UploadFile로 반환.
+    public UploadFile storeFile(MultipartFile multipartFile) throws IOException {
+        if (multipartFile.isEmpty()) {
+            return null;
+        }
+
+        //사용자가 업로드한 파일이름
+        String originalFilename = multipartFile.getOriginalFilename();
+        //서버에 저장하는 파일이름
+        String storeFilename = createStoreFilename(originalFilename);
+        multipartFile.transferTo(new File(getFullPath(storeFilename)));
+
+        return new UploadFile(originalFilename, storeFilename);
+    }
+
+    private String createStoreFilename(String originalFilename) {
+        //.png 같은 확장자 가져오기 ext
+        String ext = extractExt(originalFilename);
+        String uuid = UUID.randomUUID().toString();
+        return uuid + "." + ext;
+    }
+
+    private String extractExt(String originalFilename) {
+        int pos = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(pos + 1);
+    }
+
+}
